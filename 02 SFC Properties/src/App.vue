430c8976ce06bc26e82f0049a4cf50103c0d32bd
Sara Lissette Luis Ibáñez <sara.lissette@ultebra.eu>
2018-07-25 23:06:01 +0100

    Create 02 SFC

---
+++ 02 SFC Properties/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>{{message}}</h1>
+    <hello-component
+      v-model="message"
+    />
+  </div>
+</template>
+
+<script>
+import HelloComponent from './Hello.vue';
+
+export default {
+  name: 'app',
+  components: {
+    HelloComponent,
+  },
+  data() {
+    return {
+      message: 'Hello from Vue.js',
+    };
+  },
+};
+</script>
---
+++ 02 SFC Properties/src/Hello.vue
@@ -0,0 +1,20 @@
+<template>
+  <input
+    :value="value"
+    @input="onChange($event.target.value)"
+  />
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  props: {
+    value: String,
+  },
+  methods: {
+    onChange: function(value) {
+      this.$emit('input', value);
+    },
+  },
+};
+</script>
