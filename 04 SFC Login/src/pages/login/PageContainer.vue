2caf05760d2cf870fe58d36bbd52648c4253a729
Sara Lissette Luis Ibáñez <sara.lissette@ultebra.eu>
2018-07-29 00:17:05 +0100

    Create LoginPageContainer

---
+++ 04 SFC Login/src/pages/login/PageContainer.vue
@@ -0,0 +1,44 @@
+<template>
+  <login-page
+    :login-entity="loginEntity"
+    :update-login="updateLogin"
+    :login-request="loginRequest"
+  />
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { loginRequest } from '../../rest-api/api/login';
+import { LoginEntity, createEmptyLoginEntity } from './viewModel';
+import { mapLoginEntityVmToModel } from './mappers';
+import LoginPage from './Page.vue';
+
+// export default class PageContainer extends Vue.extend({
+export default   Vue.extend({  
+  name: 'PageContainer',
+  components: {
+    LoginPage,
+  },
+  data: () => ({
+    loginEntity: createEmptyLoginEntity(),
+  }),
+  methods: {
+    updateLogin(login: string, password: string) {
+      this.loginEntity = {
+        login,
+        password,
+      };
+    },
+    loginRequest() {
+      const loginEntityModel = mapLoginEntityVmToModel(this.loginEntity);
+      loginRequest(loginEntityModel)
+        .then(() => {
+          this.$router.push('/recipe');
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+});
+</script>
