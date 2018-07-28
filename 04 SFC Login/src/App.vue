0867ab3235f133328ec79813b0ce514040fb5d96
Sara Lissette Luis Ibáñez <sara.lissette@ultebra.eu>
2018-07-29 00:52:07 +0100

    Update App.vue

--- 04 SFC Login/src/App.vue
+++ 04 SFC Login/src/App.vue
@@ -1,11 +1,16 @@
 <template>
-  <router-view />
+  <div>
+    <h1 class="well">{{ loginEntity.login }}</h1>
+    <router-view />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import { state } from './state';
 
 export default Vue.extend({
   name: 'app',
+  data: () => state,
 });
 </script>
