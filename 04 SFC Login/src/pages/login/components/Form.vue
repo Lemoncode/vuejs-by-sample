93bfec2e2843d066aa6efcee566ffb6758994901
Sara Lissette Luis Ibáñez <sara.lissette@ultebra.eu>
2018-07-29 00:26:19 +0100

    Update Form.vue

--- 04 SFC Login/src/pages/login/components/Form.vue
+++ 04 SFC Login/src/pages/login/components/Form.vue
@@ -6,6 +6,8 @@
           class="form-control"
           placeholder="e-mail"
           type="text"
+          :value="loginEntity.login"
+          @input="(e) => updateLogin(e.target.value, loginEntity.password)"
         />
       </div>
       <div class="form-group">
@@ -13,22 +15,33 @@
           class="form-control"
           placeholder="password"
           type="password"
+          :value="loginEntity.password"
+          @input="(e) => updateLogin(loginEntity.login, e.target.value)"
         />
       </div>
-      <router-link
-        to="/recipe"
+      <button
         class="btn btn-lg btn-success btn-block"
-      >
+        @click="(e) => {
+            e.preventDefault();
+            this.loginRequest();
+          }"      
+        >
         Login
-      </router-link>
+      </button>
     </form>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import Vue, { PropOptions } from 'vue';
+import { LoginEntity } from '../viewModel';
 
 export default Vue.extend({
   name: 'FormComponent',
+  props: {
+    loginEntity: {} as PropOptions<LoginEntity>,
+    updateLogin: {} as PropOptions<(login: string, password: string) => void>,
+    loginRequest: {} as PropOptions<() => void>,
+  },  
 });
 </script>
