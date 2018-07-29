0afa1eb87154c7f0db52a661c075cbc64ccc654c
Sara Lissette Luis Ibáñez <sara.lissette@ultebra.eu>
2018-07-29 13:11:30 +0100

    Remove state and undo updates

--- 04 SFC Login/src/App.vue
+++ 04 SFC Login/src/App.vue
@@ -1,16 +1,11 @@
 <template>
-  <div>
-    <h1 class="well">{{ loginEntity.login }}</h1>
-    <router-view />
-  </div>
+  <router-view />
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import { state } from './state';
 
 export default Vue.extend({
   name: 'app',
-  data: () => state,
 });
 </script>
