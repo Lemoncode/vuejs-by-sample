c4dd5477bd4bee5c7b5a47f04dbb8158d944db35
Sara Lissette Luis Ibáñez <sara.lissette@ultebra.eu>
2018-07-29 20:29:56 +0100

    Update Page.vue

--- 06 SFC Edit Recipe/src/pages/recipe/edit/Page.vue
+++ 06 SFC Edit Recipe/src/pages/recipe/edit/Page.vue
@@ -1,14 +1,39 @@
 <template>
-  <h1> Edit Recipe Page {{ id }}</h1>
+  <div>
+    <form-component
+      :recipe="recipe"
+      :updateRecipe="updateRecipe"
+      :addIngredient="addIngredient"
+      :removeIngredient="removeIngredient"
+      :save="save"
+    />
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import Vue, { PropOptions } from 'vue';
+import { Recipe } from './viewModel';
+import { FormComponent } from './components';
+
+interface Props {
+  recipe: PropOptions<Recipe>;
+  updateRecipe: PropOptions<(field, value) => void>;
+  addIngredient: PropOptions<(ingredient) => void>;
+  removeIngredient: PropOptions<(ingredient) => void>;
+  save: PropOptions<() => void>;
+}
 
 export default Vue.extend({
   name: 'EditRecipePage',
+  components: {
+    FormComponent,
+  }
   props: {
-    id: String,
-  },  
+    recipe: {},
+    updateRecipe: {},
+    addIngredient: {},
+    removeIngredient: {},
+    save: {},
+  } as Props,
 });
 </script>
