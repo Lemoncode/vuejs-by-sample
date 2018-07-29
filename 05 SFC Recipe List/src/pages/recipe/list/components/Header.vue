15cf133a3b228d7029fb8883b8a768716e932de3
Sara Lissette Luis Ibáñez <sara.lissette@ultebra.eu>
2018-07-29 15:06:35 +0100

    Create Header component

---
+++ 05 SFC Recipe List/src/pages/recipe/list/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <thead>
+    <th>
+      Name
+    </th>
+    <th>
+      Description
+    </th>
+    <th>
+    </th>
+  </thead>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'HeaderComponent',
+})
+</script>
