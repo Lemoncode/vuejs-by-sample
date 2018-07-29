a553044af1ada5af0ae29d54e5a242d3667ba036
Sara Lissette Luis Ibáñez <sara.lissette@ultebra.eu>
2018-07-29 20:29:30 +0100

    Create Form component

---
+++ 06 SFC Edit Recipe/src/pages/recipe/edit/components/Form.vue
@@ -0,0 +1,76 @@
+<template>
+  <form class="container">
+    <div class="row">
+      <validation-component
+        :hasError="true"
+        errorMessage="Test error"
+      >
+        <Input
+          type="text"
+          label="Name"
+          name="name"
+          :value="recipe.name"
+          :inputHandler="updateRecipe"
+        />
+      </validation-component>
+    </div>
+    <div class="row">
+      <input-button-component
+        type="text"
+        label="Ingredients"
+        name="ingredients"
+        placeholder="Add ingredient"
+        :value="ingredient"
+        :inputHandler="updateIngredient"
+        buttonText="Add"
+        buttonClassName="btn btn-primary"
+        :buttonClickHandler="addIngredient"
+      />
+    </div>
+    <div class="row">
+      <div class="form-group pull-right">
+        <button-component
+          className="btn btn-lg btn-success"
+          label="Save"
+          :clickHandler="save"
+        />
+      </div>
+    </div>
+  </form>
+</template>
+
+<script lang="ts">
+import Vue, { PropOptions } from 'vue';
+import { Recipe } from '../viewModel';
+import { ValidationComponent, InputComponent, InputButtonComponent, ButtonComponent } from '../../../../common/components/form';
+
+interface Props {
+  recipe: PropOptions<Recipe>;
+  updateRecipe: PropOptions<(field, value) => void>;
+  addIngredient: PropOptions<(ingredient) => void>;
+  removeIngredient: PropOptions<(ingredient) => void>;
+  save: PropOptions<() => void>;
+}
+
+export default Vue.extend({
+  name: 'FormComponent',
+  components: {
+    ValidationComponent, InputComponent, InputButtonComponent, ButtonComponent,
+  },
+  props: {
+    recipe: {},
+    updateRecipe: {},
+    addIngredient: {},
+    removeIngredient: {},
+    save: {},
+  } as Props,
+  data: () => ({
+    ingredient: '',
+  }),
+  methods: {
+    updateIngredient(fieldName, value) {
+      this.ingredient = value;
+    },
+  },
+})
+</script>
