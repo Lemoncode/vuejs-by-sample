7cf9664e99044b5b9aa82a6a91d7f9b726a4d3a6
Sara Lissette Luis Ibáñez <sara.lissette@ultebra.eu>
2018-07-29 19:07:23 +0100

    Update Page

--- 05 SFC Recipe List/src/pages/recipe/list/Page.vue
+++ 05 SFC Recipe List/src/pages/recipe/list/Page.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="container-fluid">
     <h2>Recipes</h2>
+    <search-bar-component
+      :search-text="searchText"
+      :search-input-handler="searchInputHandler"
+    />
     <table class="table table-striped">
       <header-component />
       <tbody>
-        <template v-for="recipe in recipes">
+        <template v-for="recipe in filteredRecipes">
           <row-component
             :key="recipe.id"
             :recipe="recipe"
@@ -18,15 +22,31 @@
 <script lang="ts">
 import Vue, { PropOptions } from 'vue';
 import { Recipe } from './viewModel';
-import { HeaderComponent, RowComponent } from './components';
+import { HeaderComponent, RowComponent, SearchBarComponent } from './components';
+import { filterRecipesByCommaSeparatedText } from './business/filterRecipeBusiness';
 
 export default Vue.extend({
   name: 'RecipeListPage',
   components: {
-    HeaderComponent, RowComponent,
+    HeaderComponent, RowComponent, SearchBarComponent,
   },
   props: {
     recipes: {} as PropOptions<Recipe[]>,
-  },  
+  },
+  data() {
+    return {
+      searchText: '',
+    };
+  },
+  methods: {
+    searchInputHandler(value: string) {
+      this.searchText = value;
+    },
+  },
+  computed: {
+    filteredRecipes(): Recipe[] {
+      return filterRecipesByCommaSeparatedText(this.recipes, this.searchText);
+    },
+  },
 });
 </script>
