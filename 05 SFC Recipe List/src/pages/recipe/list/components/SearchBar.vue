8ba083db6a98dd55b10173501690dfdc672fcd80
Sara Lissette Luis Ibáñez <sara.lissette@ultebra.eu>
2018-07-29 18:50:13 +0100

    Create SearchBar component

---
+++ 05 SFC Recipe List/src/pages/recipe/list/components/SearchBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <input-component
+    type="text"
+    name="searchText"
+    :value="searchText"
+    placeholder="Search for ingredients comma separated..."
+    :inputHandler="inputHandler"
+/>
+</template>
+
+<script lang="ts">
+import Vue, { PropOptions } from 'vue';
+import { InputComponent } from '../../../../common/components/form';
+
+export default Vue.extend({
+  name: 'SearchBarComponent',
+  components: {
+    InputComponent,
+  },
+  props: {
+    searchText: String,
+    searchInputHandler: {} as PropOptions<(value: string) => void>,
+  },
+  methods: {
+    inputHandler(field: string, value: string) {
+      this.searchInputHandler(value);
+    }
+  }    
+});
+</script>
