f79ff082412e794ec1502579f4ee3436ffdf9e66
Sara Lissette Luis Ibáñez <sara.lissette@ultebra.eu>
2018-07-29 13:10:13 +0100

    Create Button component

---
+++ 04 SFC Login/src/common/components/form/Button.vue
@@ -0,0 +1,31 @@
+<template>
+  <button
+    :class="className"
+    :type="type"
+    @click="onClick"
+    :disabled="disabled"
+  >
+    {{ label }}
+  </button>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'ButtonComponent',
+  props: [
+    'className',
+    'type',
+    'label',
+    'clickHandler',
+    'disabled',
+  ],
+  methods: {
+    onClick(e) {
+      e.preventDefault();
+      this.clickHandler();
+    }
+  },
+});
+</script>
