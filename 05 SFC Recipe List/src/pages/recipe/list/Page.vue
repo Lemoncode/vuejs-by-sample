1f4b622c20763d244d7ea67dbd624e000a50a2d6
Sara Lissette Luis Ibáñez <sara.lissette@ultebra.eu>
2018-07-29 18:43:53 +0100

    Update RecipeListPage

--- 05 SFC Recipe List/src/pages/recipe/list/Page.vue
+++ 05 SFC Recipe List/src/pages/recipe/list/Page.vue
@@ -1,11 +1,32 @@
 <template>
-  <h1>Recipe List Page </h1>
+  <div class="container-fluid">
+    <h2>Recipes</h2>
+    <table class="table table-striped">
+      <header-component />
+      <tbody>
+        <template v-for="recipe in recipes">
+          <row-component
+            :key="recipe.id"
+            :recipe="recipe"
+          />
+        </template>
+      </tbody>
+    </table>
+  </div>  
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import Vue, { PropOptions } from 'vue';
+import { Recipe } from './viewModel';
+import { HeaderComponent, RowComponent } from './components';
 
 export default Vue.extend({
   name: 'RecipeListPage',
+  components: {
+    HeaderComponent, RowComponent,
+  },
+  props: {
+    recipes: {} as PropOptions<Recipe[]>,
+  },  
 });
 </script>
