c2d677c415c6e5f9dbc97276986b85ac8f19144b
Sara Lissette Luis Ibáñez <sara.lissette@ultebra.eu>
2018-07-29 21:35:25 +0100

    Update Form

--- 06 SFC Edit Recipe/src/pages/recipe/edit/components/Form.vue
+++ 06 SFC Edit Recipe/src/pages/recipe/edit/components/Form.vue
@@ -28,6 +28,17 @@
       />
     </div>
     <div class="row">
+      <validation-component
+        :hasError="true"
+        errorMessage="Test error"
+      >
+        <ingredient-list-component
+          :ingredients="recipe.ingredients"
+          :removeIngredient="removeIngredient"
+        />
+      </validation-component>
+    </div>
+    <div class="row">
       <div class="form-group pull-right">
         <button-component
           className="btn btn-lg btn-success"
@@ -43,6 +54,7 @@
 import Vue, { PropOptions } from 'vue';
 import { Recipe } from '../viewModel';
 import { ValidationComponent, InputComponent, InputButtonComponent, ButtonComponent } from '../../../../common/components/form';
+import IngredientListComponent from './IngredientList.vue';
 
 interface Props {
   recipe: PropOptions<Recipe>;
@@ -55,7 +67,7 @@ interface Props {
 export default Vue.extend({
   name: 'FormComponent',
   components: {
-    ValidationComponent, InputComponent, InputButtonComponent, ButtonComponent,
+    ValidationComponent, InputComponent, InputButtonComponent, ButtonComponent, IngredientListComponent,
   },
   props: {
     recipe: {},
