bc12797bc40e1bd2b33fc13b7a5807f5891d386f
Sara Lissette Luis Ibáñez <sara.lissette@ultebra.eu>
2018-07-29 20:07:44 +0100

    Create common components

---
+++ 06 SFC Edit Recipe/src/common/components/form/InputButton.vue
@@ -0,0 +1,55 @@
+<template>
+  <div :class="`form-group ${className}`">
+    <label :for="name">
+      {{ label }}
+    </label>
+    <div class="input-group">
+      <input
+        class="form-control"
+        :placeholder="placeholder"
+        :type="type"
+        :value="value"
+        :name="name"
+        @input="onInput"
+      />
+      <div class="input-group-btn">
+        <button
+          :class="buttonClassName"
+          @click="onButtonClick"
+        >
+          {{ buttonText }}
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'InputButtonComponent',
+  props: [
+    'className',
+    'placeholder',
+    'type',
+    'value',
+    'inputHandler',
+    'label',
+    'name',
+    'buttonText',
+    'buttonClickHandler',
+    'buttonClassName',
+  ],
+  methods: {
+    onInput(e) {
+      this.inputHandler(e.target.name, e.target.value);
+    },
+    onButtonClick(e) {
+      e.preventDefault();
+      this.buttonClickHandler(this.value);
+    },
+  },
+});
+</script>
+
