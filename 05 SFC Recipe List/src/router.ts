7f02adffebff7ff858ca71319181ec57c04e7223
Sara Lissette Luis Ibáñez <sara.lissette@ultebra.eu>
2018-07-29 18:45:11 +0100

    Update router.ts

--- 05 SFC Recipe List/src/router.ts
+++ 05 SFC Recipe List/src/router.ts
@@ -1,11 +1,11 @@
 import Router, { RouteConfig } from 'vue-router';
 import { PageContainer } from './pages/login';
-import { RecipeListPage } from './pages/recipe/list';
+import { RecipeListPageContainer } from './pages/recipe/list';
 
 const routes: RouteConfig[] = [
   { path: '/', redirect: '/login' },
   { path: '/login', component: PageContainer },
-  { path: '/recipe', component: RecipeListPage },
+  { path: '/recipe', component: RecipeListPageContainer },
 ];
 
 export const router = new Router({
