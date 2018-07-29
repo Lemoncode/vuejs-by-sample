94c05ac3c1fc0441203fc7e10893277799c48d77
Sara Lissette Luis Ibáñez <sara.lissette@ultebra.eu>
2018-07-29 20:36:13 +0100

    Create IngredienteRow component

---
+++ 06 SFC Edit Recipe/src/pages/recipe/edit/components/IngredientRow.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="col-sm-3">
+    <label class="col-xs-8">
+      {{ ingredient }}
+    </label>
+    <span
+      class="btn btn-default"
+      @click="onClick"
+    >
+      <i class="glyphicon glyphicon-remove"></i>
+    </span>
+  </div>  
+</template>
+
+<script lang="ts">
+import Vue, { PropOptions } from 'vue';
+
+export default Vue.extend({
+  name: 'IngredientRowComponent',
+  props: {
+    ingredient: String,
+    removeIngredient: {} as PropOptions<(ingredient) => void>,
+  },
+  methods: {
+    onClick() {
+      this.removeIngredient(this.ingredient);
+    },
+  },
+});
+</script>
+
