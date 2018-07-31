02ed023a0ffa2f470693767c58b9e8b5c999c428
Sara Lissette Luis Ibáñez <sara.lissette@ultebra.eu>
2018-07-31 12:58:23 +0100

    Reviewed 01 SFC sample

--- 01 SFC Hello VueJS/src/main.ts
+++ 01 SFC Hello VueJS/src/main.ts
@@ -1,8 +1,11 @@
 import Vue from 'vue';
-
 import App from './App.vue';
 
 new Vue({
   el: '#root',
-  render: h => h(App)
+  template: '<App/>',
+  components: { App },
+  data: {
+    message: 'Hello from Vue.js',
+  },
 });
